e9b36a6e1ea38b5d47210341a16f165e1e3a61fa
Amir <slampas@hotmail.fr>
2021-03-23 13:20:49 +0100

    add pokemons.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Pokemon } from './pokemons';
   
 @Component({
   selector: 'pokemon-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+    name = 'Angular'; 
+    private pokemon : Pokemon[];
+
+}
