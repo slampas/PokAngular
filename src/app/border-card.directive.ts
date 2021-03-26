a8e65aa66e57d9538fcbda1e2d5b41ab78f11aec
Amir <slampas@hotmail.fr>
2021-03-26 16:38:52 +0100

    add directive + constructeur css  border

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,17 +1,24 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { reduce } from 'rxjs-compat/operator/reduce';
   
 @Directive({
   selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
+    private initialColor: string = '#f5f5f5' ;
+    private defaultColor: string = '#009688' ;
+    private defaultHeiht: number = 180 ;
+
     constructor(private el: ElementRef) {
-        this.setBorder('#f5f5f5');
-        this.setHeight(180);
+        this.setBorder(this.initialColor);
+        this.setHeight(this.defaultHeiht);
     }
   
+    @Input ('pkmnBorderCard') borderColor: string; //alias
+    //@Input ('pkmnBorderCard') borderColor: string;  sans aliave
 
     @HostListener ('mouseenter') onMouseEnter(){
-        this.setBorder('#009688');
+        this.setBorder(  this.borderColor || '#009688');
     }
     @HostListener ('mouseleave') onMouseLeave(){
         this.setBorder('#f5f5f5');
--- src/dist/app/border-card.directive.js
+++ src/dist/app/border-card.directive.js
@@ -13,11 +13,15 @@ var core_1 = require("@angular/core");
 var BorderCardDirective = /** @class */ (function () {
     function BorderCardDirective(el) {
         this.el = el;
-        this.setBorder('#f5f5f5');
-        this.setHeight(180);
+        this.initialColor = '#f5f5f5';
+        this.defaultColor = '#009688';
+        this.defaultHeiht = 180;
+        this.setBorder(this.initialColor);
+        this.setHeight(this.defaultHeiht);
     }
+    //@Input ('pkmnBorderCard') borderColor: string;  sans aliave
     BorderCardDirective.prototype.onMouseEnter = function () {
-        this.setBorder('#009688');
+        this.setBorder(this.borderColor || '#009688');
     };
     BorderCardDirective.prototype.onMouseLeave = function () {
         this.setBorder('#f5f5f5');
@@ -29,6 +33,10 @@ var BorderCardDirective = /** @class */ (function () {
     BorderCardDirective.prototype.setHeight = function (height) {
         this.el.nativeElement.style.height = height + 'px';
     };
+    __decorate([
+        core_1.Input('pkmnBorderCard'),
+        __metadata("design:type", String)
+    ], BorderCardDirective.prototype, "borderColor", void 0);
     __decorate([
         core_1.HostListener('mouseenter'),
         __metadata("design:type", Function),
